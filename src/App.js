308dd866cc3ac8e8410db7f90c16f61458b06446
leejongwoo <leejongwoo.dev@gmail.com>
2020-10-03 16:18:22 +0900

    Fix react hot loading error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello docker
         </a>
       </header>
     </div>
